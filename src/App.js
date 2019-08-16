51bc693f90c4f85f4cabd4433cca74c8c88b2eb5
Romane G <romane71193@gmail.com>
2019-08-16 18:59:05 -0400

    made button and title

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Which MacBook</h1>
+ <Button variant="contained" color="primary" >Submit</Button>
+    </div>
+  );
+}
+
+export default App;
